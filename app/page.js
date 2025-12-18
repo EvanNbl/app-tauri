46bb411d13dc5cab8bcd0a9218f3239a0e8d8e18
EvanNbl <evannbl.pro@gmail.com>
2025-12-18 18:54:05 +0100

    fix: revert version to 0.0.2 in package.json and tauri.conf.json; update layout in page.js

--- app/page.js
+++ app/page.js
@@ -3,8 +3,11 @@ import Updater from '../components/updater'
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen items-center justify-center">
+    <main className="flex min-h-screen flex-col items-center justify-center p-24">
       <h1 className="text-4xl font-bold">Hello World</h1>
+      <p className="mt-4 text-lg">
+        version 0.0.2
+      </p>
       <Updater />
     </main>
   )
